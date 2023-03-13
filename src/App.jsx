4eacc7d296470aed09be41be4b68ab4ee940e725
fmfahath <fmfahath.dev@gmail.com>
2023-03-14 00:59:36 +0530

    NavBar links added / liks variable impoter from data.js / NavLink rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Rout, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Home from './pages/home/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
       <NavBar/>
-      <Home/>
     </BrowserRouter>
     
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../images/logo.png'
+import {links} from '../data.js'
 import './navbar.css'
 
 
@@ -7,9 +8,23 @@ const NavBar = () => {
   return (
     <nav>
         <div className='container nav_container'>
+            {/* Logo */}
             <Link to='/' className='logo'>
                 <img src={Logo} alt='nav-logo' />
             </Link> 
+
+            {/* nav links */}
+            <ul className='nav_links'>
+                {
+                    links.map(({name, path}, index) => {
+                        return(
+                            <li key={index}>
+                                <NavLink to={path}>{name}</NavLink>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div> 
     </nav>
   )
